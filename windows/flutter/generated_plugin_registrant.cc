fd427de07f24299785d983b4318f68859ff94a5a
hua <2838666797@qq.com>
2022-03-09 14:55:21 +0800

    add dll

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <reader_parser2/reader_parser2_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  ReaderParser2PluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ReaderParser2Plugin"));
+}
